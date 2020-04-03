ce7ea7526d3ff19e199321a8d38799f8f2bbdca2
panagop <gpanagop@gmail.com>
2020-04-03 04:23:54 +0300

    added markdown

--- source/conf.py
+++ source/conf.py
@@ -22,13 +22,18 @@ sys.path.append(os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['sphinx.ext.mathjax',
+  'sphinxcontrib.plantuml']
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
 
+source_parsers = {
+   '.md': 'recommonmark.parser.CommonMarkParser',
+}
+
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8'
